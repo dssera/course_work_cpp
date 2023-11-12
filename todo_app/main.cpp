248bd995cfa57054d95544541086225fed3505b0
dssera <adesser001@mail.ru>
2023-11-12 21:24:29 +0300

    separate User and AuthScreen classes

--- todo_app/main.cpp
+++ todo_app/main.cpp
@@ -11,6 +11,26 @@
 +шифровка
 */
 
+//classes
+/*
+Task-> TaskTree
+adminScreen (<-)-> userScreen
+Auth
+User
+Menu(screen)
+
+1. Auth obj is created 
+2. User Screen or Admin Screen is created (Auth obj can be deleted)
+
+Следи за временем жизни объектов
+
+				-> User - >work with tasks
+start -> auth -| 
+				-> Admin -> work with db + work with users
+
+
+*/
+
 #include <iostream>
 
 #define MENU_STRING "todo-app menu"
@@ -26,50 +46,17 @@ using namespace std;
 
 class BaseUser
 {
-private:
+public:
 	string username;
 	string password;
 	bool is_authenticated;
 
-	void identificate()
-	{
-		// вообще то здесь мы должны чекнуть есть ли наше имя в БД
-		// get_username
-
-		// видит ли эта функция this?
-		if (PLUG != this->username)
-			throw exception("identification error: \
-				bool identificate(string username)");
-	}
 protected:
 	BaseUser(string username, string password)
 	{
 		// add validation
-		// add saving in file
 		this->username = username;
 		this->password = password;
-	}
-
-	
-public:
-	void auth()
-	{
-		// signature will be changed when you add try-catch, you need to return smth to show that user is not authenticated 
-		// but you don't want to close all application
-		identificate();
-		// get_password
-		string password_from_db = PLUG;
-		if (password_from_db != this->password)
-		{
-			this->is_authenticated = false;
-			throw exception("auth error : \
-				bool auth(string username, string password)");
-		}
-		this->is_authenticated = true;
-	}
-	bool is_authenticated()
-	{
-		return this->is_authenticated;
 	}
 };
 
@@ -82,7 +69,7 @@ class User : public BaseUser
 		this->access_level = access_level;
 	}
 public:
-	User(string username, string password, string access_level=USER_ACCESS_LEVEL)
+	User(string username, string password, string access_level = USER_ACCESS_LEVEL)
 		: BaseUser(username, password)
 	{
 		this->access_level = access_level;
@@ -92,83 +79,139 @@ public:
 	{
 		return this->access_level;
 	}
-
 };
 
-// should I make this func like bool sign_in() or void sign_in()
-void sign_in()
+
+class AuthScreen
 {
-	string username;
-	string password;
-	cout << "Enter username" << endl;
-	cout << "Enter paswword" << endl;
-	cin >> username;
-	cin >> password;
-
-	User user = User(username, password);
-	user.auth();
-}
-
-void sign_up()
-{
-	cout << "Create new user" << endl;
-	string username;
-	string password;
-	cout << "Enter username" << endl;
-	cout << "Enter paswword" << endl;
-	cin >> username;
-	cin >> password;
-	//password validation
-
-	//add user in db or raise an error
-}
-
-
-
-void print_menu()
-{
-	cout << MENU_STRING << endl;
-	cout << "1." << SIGN_UP << endl;
-	cout << "2." << SIGN_IN << endl;
-}
-
-
-// можно завязать всю работу авторизации и регистрации на поинтере user 
-void auth_menu(int enter)
-{
-	bool authenticated;
-	User* user = nullptr;
-	switch (enter)
+public:
+	User* run()
 	{
-	case(1):
-		cout << SIGN_IN << endl;
-		// user = sign_in();
-		sign_in();
-		break;
-	case(2):
-		cout << SIGN_UP << endl;
-		sign_up();
-		break;
-	case(0):
-		system("exit");
-		break;
-	default:
-		break;
+		User* user = nullptr;
+		do
+		{
+			user = auth_menu();
+		} while (user == nullptr);
+
+		return user;
 	}
-}
+protected:
+	User* sign_in()
+	{
+		string username;
+		string password;
+		cout << "Enter username" << endl;
+		cout << "Enter paswword" << endl;
+		cin >> username;
+		cin >> password;
+
+		User* user = auth(username, password);
+		return user;
+	}
+
+	User* sign_up()
+	{
+		cout << "Create new user" << endl;
+		string username, password;
+		cout << "Enter username" << endl;
+		cin >> username;
+		cout << "Enter paswword" << endl;
+		cin >> password;
+		//password validation
+
+		User* user_from_db = new User("user1", "password");
+
+		//add user in db or raise an error
+		//and then retutn pointer on gotten user
+		return user_from_db;
+	}
+
+	void identificate(string username)
+	{
+		// вообще то здесь мы должны чекнуть есть ли наше имя в БД
+		// get_username
+
+		// видит ли эта функция this?
+		// if (PLUG != username)
+		if ("dssera" != username)
+			throw exception("identification error: \
+				bool identificate(string username)");
+	}
+
+	User* auth(string username, string password)
+	{
+		// signature will be changed when you add try-catch, you need to return smth to show that user is not authenticated 
+		// but you don't want to close all application
+		identificate(username);
+		// get_password
+		//string password_from_db = PLUG;
+		string password_from_db = "12345";
+		if (password_from_db != password)
+		{
+			// this->is_authenticated = false;
+			throw exception("auth error : \
+				bool auth(string username, string password)");
+		}
+		
+		User* user_from_db = new User("name1", "password");
+		return user_from_db;
+		// this->is_authenticated = true;
+	}
+	User* auth_menu()
+	{
+		bool authenticated;
+		int enter;
+		User* user = nullptr;
+		cout << "Auth Menu" << endl << "1.Sign In" << endl 
+			<< "2.Sign Up" << endl << "0.Exit" << endl;
+		
+
+		cout << "Your enter: ";
+		cin >> enter;
+
+		switch (enter)
+		{
+		case(1):
+			cout << SIGN_IN << endl;
+			// user = sign_in();
+			user = sign_in();
+			break;
+		case(2):
+			cout << SIGN_UP << endl;
+			user = sign_up();
+			break;
+		case(0):
+			system("exit");
+			break;
+		default:
+			break;
+		}
+		return user;
+	}
+};
+
+
 
 int main()
 {
-	int enter;
-	do
-	{
-		print_menu();
-		cout << "Your enter: ";
-		cin >> enter;
-		auth_menu(enter);
+	/*
+	AuthScreen obj;
+	obj.CheckOut(); 
+	*/
+	// сейчас у меня эта тема рабоатет череез функцию auth_menu
+	AuthScreen* auth_obj = new AuthScreen();;
+	User* user = auth_obj->run();
 
-		system("cls");
-	} while (enter);
+	if (user->get_access_level() == ADMIN_ACCESS_LEVEL)
+	{
+		cout << ADMIN_ACCESS_LEVEL << endl;
+	}
+	else if (user->get_access_level() == USER_ACCESS_LEVEL)
+	{
+		cout << USER_ACCESS_LEVEL << endl;
+
+	}
+
 
 	return 0;
 }
